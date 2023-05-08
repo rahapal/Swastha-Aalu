2b30d0f46aa47f0ff52515532d6a2c5391d2a19c
Sushant Rahapal <sushantrahapal@gmail.com>
2023-05-08 16:53:34 +0545

    Bottomnavbar fields

--- project/lib/screens/bottomNavBar.dart
+++ project/lib/screens/bottomNavBar.dart
@@ -15,8 +15,65 @@ class _BottomNavBarState extends State<BottomNavBar> {
     Demo(),
     Demo(),
   ];
+  final PageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = Demo();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: PageStorage(
+        bucket: bucket,
+        child: currentScreen,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.camera_alt),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 18,
+        child: SizedBox(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Demo();
+                        currentTab = 0;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.home,
+                          color:
+                              currentTab == 0 ? Colors.blueAccent : Colors.grey,
+                        ),
+                        Text(
+                          'Home',
+                          style: TextStyle(
+                            color: currentTab == 0
+                                ? Colors.blueAccent
+                                : Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
